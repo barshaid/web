f7cd36e644547257afd08d991c482a817dd5837c
barshaid <48483691+barshaid@users.noreply.github.com>
2019-05-24 23:01:37 +0300

    HW2
    Added JS
    missing modal and image carousel

---
+++ indexJS.js
@@ -0,0 +1,31 @@
+function incDesc(desc) {
+    document.getElementById(desc).style.fontSize = "xx-large";
+    document.getElementById(desc).style.fontWeight = "bold";
+    document.addEventListener("mouseout", function () {
+        document.getElementById(desc).style.fontSize = "initial";
+        document.getElementById(desc).style.fontWeight = "initial";
+    })
+}
+
+function hlCell() {
+    var tdLen = document.getElementsByTagName("td").length;
+    for (i = 0; i < tdLen; i++) {
+        const tmp = i;
+        document.getElementsByTagName("td")[tmp].addEventListener("mouseover", function () {
+            document.getElementsByTagName("td")[tmp].style.background = "rgba(69,69,69,0.48)";
+        })
+        document.getElementsByTagName("td")[tmp].addEventListener("mouseout", function () {
+            document.getElementsByTagName("td")[tmp].style.background = "initial";
+        })
+    }
+}
+
+function count() {
+    document.getElementById('textarea').onkeyup = function () {
+        document.getElementById('chars').innerHTML = (200 - this.value.length);
+    };
+
+}
+function goodbye() {
+    window.alert("Bye-bye!");
+}
